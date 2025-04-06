166ed7a35da5cc934927091587266bb40cfeb7cd
Joker <puebloseko@gmail.com>
2025-04-06 18:09:13 +0200

    fetch a la api del tiempo y muestra la T de BCN en pantalla

---
+++ dist/clima.js
@@ -0,0 +1,16 @@
+"use strict";
+const clima = document.getElementById("clima");
+let TU_API_KEY = "f8f9fc72c29078e5fd330dc83c7ee6ca";
+iniciar();
+function iniciar() {
+    //let API = `https://openweathermap.org/find?q=barcelona`;
+    let API = `https://api.openweathermap.org/data/2.5/weather?q=barcelona&appid=${TU_API_KEY}&units=metric`;
+    fetch(API)
+        .then((response) => response.json())
+        .then((data) => {
+        console.log(data);
+        console.log(data.main.temp);
+        console.log(data.wind.speed);
+        clima.innerText = data.main.temp + " C";
+    });
+}
