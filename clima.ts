b168f98b41cb59e87dbe085e57e1ca5cbd285445
Joker <puebloseko@gmail.com>
2025-04-13 21:56:52 +0200

    se cambia el tamaño de los botones ; se refactoriza el codigo que da el clima según la ubicación separandolo en 2 archivos ; se refactoriza codigo duplicado en pequeñas  funciones ; se elimina scroll vertical innecesario

--- clima.ts
+++ clima.ts
@@ -1,25 +1,28 @@
 
 import { uso_api } from "./API.js";
+import { conseguir_ubi } from "./geolocalizacion.js";
 
 const clima=document.getElementById("clima")!;
-const TU_API_KEY="f8f9fc72c29078e5fd330dc83c7ee6ca";
 
-let latitude : number;
-let longitude : number;
-let latitudeFormateado:number;
-let longitudeFormateado:number;
-
-let url : string="";
+let url : string=" ";
 let apiClima = new uso_api(url);
 
 
 
 
 
+
+
+
+
 export async function mostrar_clima(){
-
-await conseguir_ubi();
+try{
+url = await conseguir_ubi();
+apiClima.url = url;
 muestra_clima();
+}catch{
+
+}
 
 }
 
@@ -28,27 +31,6 @@ muestra_clima();
 
 
 
-function conseguir_ubi(){
-  return new Promise<void>((resolve, reject) => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-         latitude  = position.coords.latitude;
-         latitudeFormateado=Math.round(latitude * 100) / 100;
-         longitude = position.coords.longitude;
-         longitudeFormateado = Math.round(longitude * 100) / 100;
-         apiClima.url=`https://api.openweathermap.org/data/2.5/weather?lat=${latitudeFormateado}&lon=${longitudeFormateado}&appid=${TU_API_KEY}&units=metric`
-
-        resolve();
-      }, function(error) {
-        reject("Error al obtener la ubicación: " + error.message);
-      });
-    } else {
-      console.warn("La geolocalización no está soportada por este navegador.");
-    }
-  });
- 
-
-}
 
 async function muestra_clima(){
   let data = await apiClima.obten_datos();
